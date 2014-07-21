7201b8bd8e96305239ea17bf964f9072e354f9d9
Noodep <noodverse@gmail.com>
2014-07-21 17:29:49 -0500

    Added a bunch of comments in a jsdoc format, updated constructor with options

--- gobmods/webgl.js
+++ gobmods/webgl.js
@@ -8,26 +8,43 @@
 	// Defines local factory
 	var WEBGLContext = {};
 
-	// Creates and returns a simple context
-	WEBGLContext.create = function(canvas, options) {
-		options = options || {};
-
-		var canvas = _.G(canvas);
-
-		return new WGLC(canvas);
+	/**
+	 * Creates an instance of a WEBGLContext.
+	 *
+	 * @param	{string}	canvas	The id of the canvas on which the context will be instanciated.
+	 * @throws	{Error}		If canvas element with id canvas_id doesn't exist.
+	 * @return	{WGLC}		The new WGLC object.
+	 */
+	WEBGLContext.create = function(canvas_id) {
+		var canvas = _.G(canvas_id);
+		if(canvas == undefined) throw new Error('Unable to find canvas element with id "' + canvas_id + '"');
+		var options = {
+			camera: new _.Camera({position: new _.v3(0,0,-2)}),
+			renderCallback: undefined
+		};
+		return new WGLC(canvas, options);
 	}
 
-
-	// Webgl Context object
-	function WGLC(canvas) {
+	/**
+	 * Creates an instance of a WEBGLContext
+	 *
+	 * @constructor
+	 * @this	{WGLC}
+	 * @param	{DOMElement}	canvas	The canvas element on which the context will be instanciated
+	 * @param	{object}		options	A javascript object containing the context parameters
+	 *									- camera
+	 *									- renderCallback
+	 * @return {WGLC} The new WGLC object.
+	 */
+	function WGLC(canvas, options) {
 		this._canvas = canvas;
+		this._camera = options.camera;
+		this._angle_y = 0;
 
 		if(!this._canvas)
 			throw new Error('Unable to retreive the provided canvas element :' + canvas);
 
 		this._context = this.c = this.initContext();
-		if(!this._context)
-			throw new Error('Unable to create webgl context');
 
 		this._activeBuffer = null;
 		this._vbos = [];
@@ -37,15 +54,33 @@
 		this._model_mat = _.m4.identity();
 	}
 
-	//	Creates a shader program with given name and associates it with current context object
-	//	if no path is specified the function will look for them under the folder :
-	//	DEFAULT_SHADER_PATH/name/name.(vert|frag)
-	//	This function creates and loads the program. It adds an entry to the loading queue.
-	//	if you specify a callback, it will be call when the program is ready
-	//	Options format:
-	//		name 					= name of the program to create. Must be unique.
-	//		path (optional)			= path under which the function will look for the shaders.
-	//		callback (optional)		= this callback will be called when the program is ready.
+	/**
+	 * Initialize the context attribute with an instance of a webgl canvas context 
+	 *
+	 * @this{WGLC}
+	 * @throws {Error} If unable to create a webgl context.
+	 */
+	WGLC.prototype.initContext = function() {
+		this._context = this._canvas.getContext('webgl') || this._canvas.getContext('experimental-webgl');
+		if(!this._context)
+			throw new Error('Unable to create webgl context');
+	};
+
+
+	/**
+	 * Creates a shader program with given name and associates it with current context object
+	 * if no path is specified the function will look for them under the folder :
+	 * DEFAULT_SHADER_PATH/name/name.(vert|frag)
+	 * This function creates and loads the program. It adds an entry to the loading queue.
+	 * if you specify a callback, it will be call when the program is ready
+	 * 
+	 * @param  {object} options
+	 *		name 					Name of the program to create. Must be unique.
+	 *		path (optional)			Path under which the function will look for the shaders.
+	 *		callback (optional)		Callback to be called when the program is ready.
+	 *
+	 * @throws {Error}	If name of the shader isn't specified.
+	 */
 	WGLC.prototype.createProgram = function(options) {
 		options = options || {};
 		if(!options.name) throw new Error('you need to specify a name for your program');
@@ -91,9 +126,15 @@
 		});
 	}
 
-	// Creates and compile a new shader object based on the given OPENGL ES source
-	// shader	= OPENGL ES source code
-	// type		= defines the type of shader to create. FRAGMENT_SHADER | VERTEX_SHADER
+	/**
+	 * Creates and compile a new shader object based on the given OPENGL ES source
+	 * @param  {string}		src		OPENGL ES source code
+	 * @param  {constant}	type 	Type of the shader to create. FRAGMENT_SHADER | VERTEX_SHADER.
+	 * 
+	 * @throws {Error} If Shader fails to compile.
+	 * 
+	 * @return {object}	The newly created shader.
+	 */
 	WGLC.prototype.createShader = function(src, type) {
 		// Compiles the shader
 		var shader = this.c.createShader(type);
@@ -106,8 +147,14 @@
 		return shader;
 	}
 
-	// Creates a new program object and attach shaders to it.
-	// This function initialize references to uniforms and attributes as specified in options
+	/**
+	 * Creates a new program object and attach shaders to it.
+	 * This function initialize references to uniforms and attributes as specified in options
+	 * 
+	 * @param {string} name	Name of the program to compile. 
+	 *
+	 * @throws {Error} If the program fails to link or validate.
+	 */
 	WGLC.prototype.compileProgram = function(name) {
 		var p = this._programs[name];
 		p.program = this.c.createProgram();
@@ -131,9 +178,16 @@
 		p.callback(p);
 	}
 
-
-	// Dynamically locates attributes or uniforms for a given program
-	// Stores pointers in the given program object 
+	/**
+	 * Dynamically locates attributes or uniforms for a given program.
+	 * Stores pointers in the given program object.
+	 * @param  {program object}	p		Program on which to look for qualifiers.
+	 * @param  {string}			type	Type of qualifier to identify
+	 * 										'u' for uniforms
+	 * 										'a' for attributes
+	 * 										
+	 * @throws {Error} 		If type of qualifier is different from 'u' or 'a'
+	 */
 	WGLC.prototype.qualify = function (p, type) {
 		var res, pname, fname;
 		if(type === 'u') {
@@ -157,22 +211,34 @@
 		}
 	}
 
-	// Changes the program currently used by this context
-	// If the specified program doesn't exists, it throws an exception.
-	WGLC.prototype.useProgram = function(program_name) {
-		var p = this.getProgram(program_name);
+	/**
+	 * Changes the program currently used by this context
+	 * @param  {string} program_id The program identifier.
+	 */
+	WGLC.prototype.useProgram = function(program_id) {
+		var p = this.getProgram(program_id);
 		this.c.useProgram(p.program);
 	};
 
-	// Returns a program identified by id program_name
-	// If the specified program doesn't exists, it throws an exception.
+	/**
+	 * Returns a program identified by the identifier program_id
+	 * @param  {string} program_id Identifier of the program to return.
+	 *
+	 * @throws {Error} If program with id program_id doesn't exist.
+	 * 
+	 * @return {program object} The program with identifier program_id
+	 */
 	WGLC.prototype.getProgram = function(program_id) {
-		var p = this._programs[program_id];
-		if(!p) throw new Error('Unknown program : ' + program_id);
+		if(!programExists(program_id)) throw new Error('Unknown program : ' + program_id);
+		var p = this._programs[program_id];		
 		return p;
 	};
 
-
+	/**
+	 * Checks if the current program with given id program_id exists.
+	 * @param  {string} program_id The identifier of the program to look for.
+	 * @return {program object}
+	 */
 	WGLC.prototype.programExists = function(program_id) {
 		return this._programs[program_id] !== undefined;
 	}
@@ -193,7 +259,13 @@
 
 	WGLC.prototype.setVBOData = function(id, data) {
 		this.makeVBOActive(id);
+		this._vbos[id].size = data.length;
 		this.c.bufferData(this.c.ARRAY_BUFFER, new Float32Array(data), this.c.STATIC_DRAW);
+	}
+
+
+	WGLC.prototype.linkUniform1f = function(uniform, value) {
+		this.c.uniform1f(uniform, value);
 	}
 
 	WGLC.prototype.linkAtribute = function(attribute) {
@@ -205,18 +277,12 @@
 		this.c.uniformMatrix4fv(uniform, false, this._projection_mat.m);
 	}
 
-	WGLC.prototype.linkModelUniform = function(uniform) {
-		this.c.uniformMatrix4fv(uniform, false, this._model_mat.m);
-	}
-
-	WGLC.prototype.render = function() {
-		this.c.drawArrays(this.c.TRIANGLE_STRIP, 0, 4);
+	WGLC.prototype.render = function(offset, size) {
+		// var size = this._vbos[this._activeBuffer].size
+		this.c.drawArrays(this.c.LINE_LOOP, offset, size);
 	}
 
 	// Initializes a new webgl context for the current object canvas : this._canvas
-	WGLC.prototype.initContext = function() {
-		return this._canvas.getContext('webgl') || this._canvas.getContext('experimental-webgl');
-	};
 
 	WGLC.prototype.setDimension = function(width, height) {
 		this._canvas.width = width;
@@ -230,25 +296,21 @@
 	};
 
 	// Model change
-	WGLC.prototype.identity = function() {
-		this._model_mat.identity();
-	};
-	WGLC.prototype.translate = function(x,y,z) {
-		this._model_mat.translate(x,y,z);
-	};
-
-	// Model change
-	WGLC.prototype.rotateZ = function(r) {
-		this._model_mat.rotateZ(r);
-	};
-
-	WGLC.prototype.rotateY = function(r) {
-		this._model_mat.rotateY(r);
-	};
-
-	WGLC.prototype.rotateX = function(r) {
-		this._model_mat.rotateX(r);
+	WGLC.prototype.getCam = function() {
+		return this._camera;
 	}
+
+	WGLC.prototype.rotateY = function(angle) {
+		this._angle_y = angle;
+	}
+
+	WGLC.prototype.updateCamera = function(delta_t) {
+		this._camera.update(delta_t);
+		var view = this._camera.getMatrix();
+		view.rotateY(this._angle_y);
+		this.c.uniformMatrix4fv(this.getProgram('context').uniforms.u_model_view_mat, false, view.m);
+	}
+
 	// Changes clear color
 	//	color = array of floats [red, green, blue, alpha]. Floats will be clamped between [0,1]
 	WGLC.prototype.setBackground = function(color) {
